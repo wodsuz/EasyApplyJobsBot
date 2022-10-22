80e559b18a3be37fdfde4f4bec63e0bee2a93c97
ongun <ongun.demirag@gmail.com>
2022-10-22 16:10:59 +0300

    updated example config file

--- .example.config.py
+++ .example.config.py
@@ -2,7 +2,8 @@
 
 # browser you want the bot to run ex: ["Firefox"], ["Chrome"] choose one only
 browser = ["Firefox"]
-
+# Optional! run browser in headless mode, no browser screen will be shown it will work in background.
+headless = False
 # Optional! for Firefox enter profile dir to run the bot without logging in your account each time
 firefoxProfileRootDir = ""
 # If you left above field empty enter your Linkedin password and username below
@@ -10,19 +11,34 @@ firefoxProfileRootDir = ""
 email = ""
 password = ""
 
-
+# These settings are for running Linkedin job apply bot
+LinkedinBotProPasswrod = ""
+# location you want to search the jobs - ex : ["Poland", "Singapore", "New York City Metropolitan Area", "Monroe County"]
+# continent locations:["Europe", "Asia", "Australia", "NorthAmerica", "SouthAmerica", "Africa", "Australia"]
+location = ["Europe"]
+# keywords related with your job search
+keywords = ["frontend", "react", "typescript","javascript", "vue", "python", "programming", "blockchain"]
+#job experience Level - ex:  ["Internship", "Entry level" , "Associate" , "Mid-Senior level" , "Director" , "Executive"]
+experienceLevels = [ "Entry level" ]
+#job posted date - ex: ["Any Time", "Past Month" , "Past Week" , "Past 24 hours"] - select only one
+datePosted = ["Past Week"]
+#job type - ex:  ["Full-time", "Part-time" , "Contract" , "Temporary", "Volunteer", "Intership", "Other"]
+jobType = ["Full-time", "Part-time" , "Contract"]
+#remote  - ex: ["On-site" , "Remote" , "Hybrid"]
+remote = ["On-site" , "Remote" , "Hybrid"]
+#salary - ex:["$40,000+", "$60,000+", "$80,000+", "$100,000+", "$120,000+", "$140,000+", "$160,000+", "$180,000+", "$200,000+" ] - select only one
+salary = [ "$80,000+"]
+#sort - ex:["Recent"] or ["Relevent"] - select only one
+sort = ["Recent"]
 
 # These settings are for running AngelCO job apply bot
 AngelCoBotPassword = ""
-
 # AngelCO credits
 email = ""
 password = ""
-
 # jobTitle ex: ["Frontend Engineer", "Marketing"]
-jobTitle = ["Frontend Engineer"]
-
+angelCoJobTitle = ["Frontend Engineer"]
 # location ex: ["Poland"]
-location = ["Poland"]
+angelCoLocation = ["Poland"]
 
 
