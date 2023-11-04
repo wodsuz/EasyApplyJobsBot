965e41263d6ce2879cb2898bca19de09d4d6a0a7
Gabriel Gircenko <gabriel.gircenko@gmail.com>
2023-11-04 20:23:25 +0100

    added 1 more try block

--- linkedin.py
+++ linkedin.py
@@ -51,99 +51,104 @@ class Linkedin:
             prRed("❌ Couldn't generate url, make sure you have /data folder and modified config.py file for your preferances.")
 
     def linkJobApply(self):
-        self.generateUrls()
-        countApplied = 0
-        countJobs = 0
+        try:
+            self.generateUrls()
+            countApplied = 0
+            countJobs = 0
 
-        urlData = utils.getUrlDataFile()
+            urlData = utils.getUrlDataFile()
 
-        for url in urlData:        
-            self.driver.get(url)
+            for url in urlData:        
+                self.driver.get(url)
 
-            try:
-                totalJobs = self.wait.until(EC.presence_of_element_located((By.XPATH, '//small'))).text # TODO - fix finding total jobs
-                # totalJobs = self.driver.find_element(By.XPATH,'//small').text 
+                try:
+                    totalJobs = self.wait.until(EC.presence_of_element_located((By.XPATH, '//small'))).text # TODO - fix finding total jobs
+                    # totalJobs = self.driver.find_element(By.XPATH,'//small').text 
 
-                totalPages = utils.jobsToPages(totalJobs)
+                    totalPages = utils.jobsToPages(totalJobs)
 
-                urlWords =  utils.urlToKeywords(url)
-                lineToWrite = "\n Search keyword: " + urlWords[0] + ", Location: " +urlWords[1] + ", Applying " +str(totalJobs)+ " jobs."
-                self.displayWriteResults(lineToWrite)
+                    urlWords =  utils.urlToKeywords(url)
+                    lineToWrite = "\n Search keyword: " + urlWords[0] + ", Location: " +urlWords[1] + ", Applying " +str(totalJobs)+ " jobs."
+                    self.displayWriteResults(lineToWrite)
 
-                for page in range(totalPages):
-                    currentPageJobs = constants.jobsPerPage * page
-                    url = url +"&start="+ str(currentPageJobs)
-                    self.driver.get(url)
-                    utils.sleepInBetweenActions()
-
-                    offersPerPage = self.driver.find_elements(By.XPATH,'//li[@data-occludable-job-id]')
-                    offerIds = []
-
-                    utils.sleepInBetweenActions()
-
-                    for offer in offersPerPage:
-                        offerId = offer.get_attribute("data-occludable-job-id")
-                        offerIds.append(int(offerId.split(":")[-1]))
-
-                    for jobID in offerIds:
-                        offerPage = 'https://www.linkedin.com/jobs/view/' + str(jobID)
-                        self.driver.get(offerPage)
+                    for page in range(totalPages):
+                        currentPageJobs = constants.jobsPerPage * page
+                        url = url +"&start="+ str(currentPageJobs)
+                        self.driver.get(url)
                         utils.sleepInBetweenActions()
 
-                        countJobs += 1
+                        offersPerPage = self.driver.find_elements(By.XPATH,'//li[@data-occludable-job-id]')
+                        offerIds = []
 
-                        jobProperties = self.getJobProperties(countJobs)
-                        if "blacklisted" in jobProperties: 
-                            lineToWrite = jobProperties + " | " + "* 🤬 Blacklisted Job, skipped!: " +str(offerPage)
-                            self.displayWriteResults(lineToWrite)
-                        
-                        else:                    
-                            button = self.easyApplyButton()
+                        utils.sleepInBetweenActions()
 
-                            if button is not False:
-                                try:
-                                    button.click()
-                                except:
-                                    self.driver.execute_script("arguments[0].click();", button)
-                                
-                                utils.sleepInBetweenActions()
-                                countApplied += 1
-                                
-                                try:
-                                    self.chooseResumeIfOffered()
-                                    self.driver.find_element(By.CSS_SELECTOR, "button[aria-label='Submit application']").click()
-                                    utils.sleepInBetweenActions()
+                        for offer in offersPerPage:
+                            offerId = offer.get_attribute("data-occludable-job-id")
+                            offerIds.append(int(offerId.split(":")[-1]))
 
-                                    lineToWrite = jobProperties + " | " + "* 🥳 Just Applied to this job: " + str(offerPage)
-                                    self.displayWriteResults(lineToWrite)
+                        for jobID in offerIds:
+                            offerPage = 'https://www.linkedin.com/jobs/view/' + str(jobID)
+                            self.driver.get(offerPage)
+                            utils.sleepInBetweenActions()
 
-                                except:
+                            countJobs += 1
+
+                            jobProperties = self.getJobProperties(countJobs)
+                            if "blacklisted" in jobProperties: 
+                                lineToWrite = jobProperties + " | " + "* 🤬 Blacklisted Job, skipped!: " +str(offerPage)
+                                self.displayWriteResults(lineToWrite)
+                            
+                            else:                    
+                                button = self.easyApplyButton()
+
+                                if button is not False:
                                     try:
-                                        self.driver.find_element(By.CSS_SELECTOR,"button[aria-label='Continue to next step']").click()
+                                        button.click()
+                                    except:
+                                        self.driver.execute_script("arguments[0].click();", button)
+                                    
+                                    utils.sleepInBetweenActions()
+                                    countApplied += 1
+                                    
+                                    try:
+                                        self.chooseResumeIfOffered()
+                                        self.driver.find_element(By.CSS_SELECTOR, "button[aria-label='Submit application']").click()
                                         utils.sleepInBetweenActions()
 
-                                        comPercentage = self.driver.find_element(By.XPATH,'html/body/div[3]/div/div/div[2]/div/div/span').text
-                                        percentNumber = int(comPercentage[0:comPercentage.index("%")])
-                                        result = self.applyProcess(percentNumber, offerPage)
-                                        lineToWrite = jobProperties + " | " + result
+                                        lineToWrite = jobProperties + " | " + "* 🥳 Just Applied to this job: " + str(offerPage)
                                         self.displayWriteResults(lineToWrite)
-                                    
-                                    except Exception as e: 
-                                        self.chooseResumeIfOffered()
-                                        lineToWrite = jobProperties + " | " + "* 🥵 Cannot apply to this Job! " + str(offerPage)
-                                        self.displayWriteResults(lineToWrite)
-                            else:
-                                lineToWrite = jobProperties + " | " + "* 🥳 Already applied! Job: " + str(offerPage)
-                                self.displayWriteResults(lineToWrite)
-                                
-            except TimeoutException:
-                print("Element not found within the time limit")
-                # TODO Handle the situation, like retrying, logging, or graceful shutdown
 
-            prYellow("Category: " + urlWords[0] + "," +urlWords[1]+ " applied: " + str(countApplied) +
-                  " jobs out of " + str(countJobs) + ".")
-        
-        utils.donate(self)
+                                    except:
+                                        try:
+                                            self.driver.find_element(By.CSS_SELECTOR,"button[aria-label='Continue to next step']").click()
+                                            utils.sleepInBetweenActions()
+
+                                            comPercentage = self.driver.find_element(By.XPATH,'html/body/div[3]/div/div/div[2]/div/div/span').text
+                                            percentNumber = int(comPercentage[0:comPercentage.index("%")])
+                                            result = self.applyProcess(percentNumber, offerPage)
+                                            lineToWrite = jobProperties + " | " + result
+                                            self.displayWriteResults(lineToWrite)
+                                        
+                                        except Exception as e: 
+                                            self.chooseResumeIfOffered()
+                                            lineToWrite = jobProperties + " | " + "* 🥵 Cannot apply to this Job! " + str(offerPage)
+                                            self.displayWriteResults(lineToWrite)
+                                else:
+                                    lineToWrite = jobProperties + " | " + "* 🥳 Already applied! Job: " + str(offerPage)
+                                    self.displayWriteResults(lineToWrite)
+                                    
+                except TimeoutException:
+                    print("Element not found within the time limit")
+                    # TODO Handle the situation, like retrying, logging, or graceful shutdown
+
+                prYellow("Category: " + urlWords[0] + "," +urlWords[1]+ " applied: " + str(countApplied) +
+                    " jobs out of " + str(countJobs) + ".")
+            
+            utils.donate(self)
+
+        except Exception as e:
+            if config.displayWarnings:
+                prYellow("⚠️ Exception caught: " +str(e)[0:50])
 
     def chooseResumeIfOffered(self):
         try: 
